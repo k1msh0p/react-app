38c19e9c700d6edd48b4cfe4af3f2ec668a5a154
Kim Chin <>
2021-10-27 23:36:40 -0400

    chapter 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from 'react'
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     { id: 'e1', title: 'Car Insurance', amount: 293.77, date: new Date(2020, 7, 14) },
     { id: 'e2', title: 'Toilet paper', amount: 12.07, date: new Date(2020, 7, 14) },
@@ -11,26 +12,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses}/>
     </div>
   );
 }
