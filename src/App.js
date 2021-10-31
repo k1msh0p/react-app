9779ccc6211189370547793082c8a842705721a5
Kim Chin <>
2021-10-31 14:18:45 -0400

    3 ways to implement useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -12,6 +13,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 //function ExpenseItem(props) {}
 const ExpenseItem = (props) => {
+  
+  //"title" is the variable, setTitle is the function to update "title"
+  const [title, setTitle] = useState(props.title);
+  console.log('eval me');
+
+  const clickHandler = () => {
+    console.log('me');
+    setTitle('come on');
+  }
+
   return (
     <Card className="expense-item">
-        <ExpenseDate date={props.date} />
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <div className="expense-item__price">${props.amount}</div>
-        </div>
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
